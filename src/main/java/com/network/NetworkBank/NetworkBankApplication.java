f9d195432e2cb4997d2f5e5f812433c1745deeac
Lucas Gabriel <lucasgcsousa2005@gmail.com>
2025-06-14 01:58:37 -0300

    First commit

---
+++ src/main/java/com/network/NetworkBank/NetworkBankApplication.java
@@ -0,0 +1,13 @@
+package com.network.NetworkBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetworkBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetworkBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/network/NetworkBank/NetworkBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.network.NetworkBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetworkBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
